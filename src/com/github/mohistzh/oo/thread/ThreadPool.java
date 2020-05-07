0d54af8047db43f8700726896c4827a9e52eeeb7
Z <mohistzh@gmail.com>
2020-05-07 21:19:48 +0800

    simple thread pool

--- src/com/github/mohistzh/oo/thread/ThreadPool.java
+++ src/com/github/mohistzh/oo/thread/ThreadPool.java
@@ -10,9 +10,26 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class ThreadPool {
     private final int threadNumber;
     private final LinkedBlockingQueue<Runnable> taskQueue;
+    private final ThreadWorker[] threads;
 
+    /**
+     * For simplify the threadNumber parameter is a core threads number
+     * @param threadNumber
+     */
     public ThreadPool(int threadNumber) {
         this.threadNumber = threadNumber;
         taskQueue = new LinkedBlockingQueue();
+        threads = new ThreadWorker[threadNumber];
+        for (int i = 0; i < threadNumber; i++) {
+            threads[i] = new ThreadWorker(taskQueue);
+            threads[i].start();
+        }
+    }
+
+    public void execute(Runnable task) {
+        synchronized (taskQueue) {
+            taskQueue.add(task);
+            taskQueue.notify();
+        }
     }
 }
