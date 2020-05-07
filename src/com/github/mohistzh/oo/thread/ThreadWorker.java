06a201c4d81dae004207d8db4925c2bd2886e049
Z <mohistzh@gmail.com>
2020-05-07 21:44:20 +0800

    enrich thread worker

--- src/com/github/mohistzh/oo/thread/ThreadWorker.java
+++ src/com/github/mohistzh/oo/thread/ThreadWorker.java
@@ -1,36 +1,56 @@
 package com.github.mohistzh.oo.thread;
 
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
+ * The underlying thread worker in our ThreadPool which does the polling and execution.
+ *
  * @Author Jonathan
  * @Date 2020/5/7
  **/
 public class ThreadWorker extends Thread {
+    /**
+     * a switcher to turn on/off the thread
+     */
+    private AtomicBoolean executable;
+    /**
+     * a unbounded blocking queue which for storing task(Runnable)
+     */
     private final LinkedBlockingQueue<Runnable> taskQueue;
-    public ThreadWorker(LinkedBlockingQueue taskQueue) {
+
+    /**
+     * initialize thread worker
+     * @param threadName
+     * @param execute
+     * @param taskQueue
+     */
+    public ThreadWorker(String threadName, AtomicBoolean execute, LinkedBlockingQueue taskQueue) {
+        super(threadName);
+        this.executable = execute;
         this.taskQueue = taskQueue;
     }
 
     public void run() {
-        Runnable task;
-        while (true) {
-            synchronized (taskQueue) {
-                while (taskQueue.isEmpty()) {
-                    try {
-                        taskQueue.wait();
-                    } catch (InterruptedException e) {
-                        System.out.println("An error occurred while task queue is waiting: " + e.getMessage());
-                    }
-
+        try {
+            /**
+             *  When the executable flag is true, or there are any task in the queue
+             */
+            while (this.executable.get() || !taskQueue.isEmpty()) {
+                Runnable task;
+                /**
+                 * Polling a task from the queue then execute it.
+                 */
+                while ((task = taskQueue.poll()) != null) {
+                    task.run();
                 }
-                task = taskQueue.poll();
+                /**
+                 * Thread.sleep(1) helps to prevent hogging the CPU in case there wasn't any task in the queue.
+                 */
+                Thread.sleep(1);
             }
-            try {
-                task.run();
-            } catch (RuntimeException e) {
-                System.out.println("Thread pool is interrupted due to the runtime exception: " + e.getMessage());
-            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
