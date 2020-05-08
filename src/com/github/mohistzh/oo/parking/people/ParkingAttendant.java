acfb45a4e4f3df988e4960eea286ffe2916133cc
Z <mohistzh@gmail.com>
2020-05-08 10:37:50 +0800

    skeleton code of Parking Attendant

---
+++ src/com/github/mohistzh/oo/parking/people/ParkingAttendant.java
@@ -0,0 +1,12 @@
+package com.github.mohistzh.oo.parking.people;
+
+/**
+ * Parking attendant
+ * @Author Jonathan
+ * @Date 2020/5/8
+ **/
+public class ParkingAttendant extends Account {
+    public boolean processTicket(String ticketNumber) {
+        return false;
+    }
+}
