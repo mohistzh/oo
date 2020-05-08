82121fa0bc80e3c4dff43f82bd6ca207b12deac7
Z <mohistzh@gmail.com>
2020-05-08 15:02:53 +0800

    customer info portal

---
+++ src/com/github/mohistzh/oo/parking/spot/CustomerInfoPortal.java
@@ -0,0 +1,17 @@
+package com.github.mohistzh.oo.parking.spot;
+
+/**
+ * Customer Information Portal
+ * @Author Jonathan
+ * @Date 2020/5/8
+ **/
+public class CustomerInfoPortal {
+    private String id;
+    public boolean scanTicket() {
+        return false;
+    }
+
+    public boolean processPayment() {
+        return false;
+    }
+}
